adf919d100517cf65135b23241908194e219a1fe
mois3222 <moisky222@gmail.com>
2023-06-15 15:21:21 -0500

    config webpack with babel done

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      inject: true,
+      template: "/public/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
