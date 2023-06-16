960cac676184902ea054b226019988421f4e8fbb
mois3222 <moisky222@gmail.com>
2023-06-16 11:53:39 -0500

    styles.css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -34,6 +35,10 @@ module.exports = {
       inject: true,
       template: "/public/index.html",
       filename: "./index.html",
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./src/styles/styles.css", to: "" }],
     }),
   ],
 };
